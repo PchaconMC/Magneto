cd9faa77bab474f34871eaeae715fd0347f49847
pchacon <pedro.chacon@manawac.co.uk>
2021-04-07 14:31:51 +0200

    Inicio pruebas de Integración.

---
+++ magneto-mutant/src/main/java/com/magneto/mutant/dto/Data.java
@@ -0,0 +1,20 @@
+package com.magneto.mutant.dto;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Data {
+	public static final StatsDto stats_001 = new StatsDto(10.0,5.0,2.0); 
+	public static final StatsDto stats_002 = new StatsDto(10.0,20.0,0.5); 
+	public static final StatsDto stats_003 = new StatsDto(25.0,5.0,5.0); 
+	public static final StatsDto stats_004 = new StatsDto(40.0,100.0,0.4); 
+	public static final List<String> dna_human_01 = Arrays.asList("ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTT");
+	public static final HumanDto humanDto_001 = new HumanDto(dna_human_01); 
+	public static final List<String> dna_human_02 = Arrays.asList("ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTA");
+	public static final HumanDto humanDto_002 = new HumanDto(dna_human_02); 
+	public static final List<String> dna_human_03 = Arrays.asList("ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG");
+	public static final HumanDto humanDto_003 = new HumanDto(dna_human_03); 
+	public static final List<String> dna_mutant_01 = Arrays.asList("ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG");
+	public static final HumanDto mutantDto_001 = new HumanDto(dna_human_01); 
+
+}
---
+++ magneto-mutant/src/test/java/com/magneto/mutant/impl/HumanImplTest.java
@@ -0,0 +1,33 @@
+package com.magneto.mutant.impl;
+
+import com.magneto.mutant.dto.StatsDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HumanImplTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getStats() {
+        //StatsDto statsDto = this.getStats();
+
+        assertEquals(1.0,0);
+
+    }
+
+    @Test
+    void isMutant() {
+    }
+}
